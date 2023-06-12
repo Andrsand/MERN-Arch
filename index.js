2e2dea167a32069a622750440e48242fce35f82d
andrsand <angorko@list.ru>
2023-06-12 13:14:33 +0300

    Creating a server and installing nodemon

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import express from "express";
 
 const app = express(); // создание приложения
+
+app.get('/', (req, res) => { // Если клиент запрости главную страницу - ответ будет Hello World.
+    res.send('Hello World');
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+})
