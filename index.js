08243c372e2413ab347cdca911b304951f3cbf9c
andrsand <angorko@list.ru>
2023-06-12 23:32:49 +0300

    We taught our application to read json

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express(); // создание приложения
 
+app.use(express.json()); // для того чтобы express мог читать формат json
+
 app.get('/', (req, res) => { // Если клиент запрости главную страницу - ответ будет Hello World.
     res.send('Hello World');
+});
+
+app.post('/auth/login', (req, res) => { // при запросе по джанному адресу дайем олтвет в json
+    res.json({
+        success: true,
+    });
 });
 
 app.listen(4444, (err) => {
