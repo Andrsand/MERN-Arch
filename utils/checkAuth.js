3e28e642b7cc2bd8ec654c37a7994e3564be4ade
andrsand <angorko@list.ru>
2023-07-01 14:48:42 +0300

    Fully implemented user authorization check functionality

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -2,9 +2,22 @@ import jwt from 'jsonwebtoken';
 
 // функция проверки информации о себе
 export default (req, res, next) => {
-    const token = req.headers.authorization;
+    const token = (req.headers.authorization || '').replace(/Bearer\s?/, ''); // если пришел токен или нет то передавать в любом случае строку. Регуляркой удаляем слово Bearer и заменяем на пустую строку.
 
-    console.log(token);
+    if (token) {                // если токен есть - то с помощью jwt.verify расшифровываем токен с ключом secret123, иначе верни сообщение со статусом 403
+        try {
+            const decoded = jwt.verify(token, 'secret123');
 
-    next();
+            req.userId = decoded._id; // если токет расшифрован то вытаскиваем в req наш _id.
+            next();                   // если все нормально то выполняется следующая функция.
+        } catch (e) {
+            return res.status(403).json({
+                message: 'Нет доступа',
+            });
+        }
+    } else {
+        return res.status(403).json({
+            message: 'Нет доступа',
+        });
+    }
 };
