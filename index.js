933e5cd7035f43b5f6fc2779fe55e4d94ad548ed
andrsand <angorko@list.ru>
2023-07-01 20:18:33 +0300

    changing the file structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from 'mongoose'
 
-import { registerValidation } from './validations/auth.js';
+import { registerValidation } from './validations.js';
 
 import * as UserController from './controllers/UserController.js'; // импортируем все методы из данного файла
 
