956527562f54a2a483e3616a8be08793f2d27e21
andrsand <angorko@list.ru>
2023-07-03 10:03:44 +0300

    Implementing the Post Creation Controller

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,6 +1,19 @@
-export const create = (req, res) => {
-    try {
+import PostModel from '../models/Post';
 
+// создание документа
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.title,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId,            // userId мы вытаскиваем уже не из body а из checkAuth.js - req.userId = decoded._id;
+        });
+
+        const post = await doc.save();   // сохраняем документ
+
+        res.json(post);                  // возвращаем ответ
     } catch (err) {
 
     }
