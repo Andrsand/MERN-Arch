11af834f2c6fbaefba4bd7a100c1bc37cef73023
andrsand <angorko@list.ru>
2023-07-05 17:32:17 +0300

    Installed multer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from 'mongoose'
+import multer from "multer";
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 
