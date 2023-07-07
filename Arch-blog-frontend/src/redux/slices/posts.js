d7bd2568a05ee8347cbbcfa879cc30ff93d3e6f2
andrsand <angorko@list.ru>
2023-07-07 10:13:41 +0300

    Created by postSlice

---
+++ Arch-blog-frontend/src/redux/slices/posts.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    post: {                      // статьи
+        items: [],
+        status: 'loading',
+    },
+    tags: {                      // тэги
+        items: [],
+        status: 'loading',
+    },
+};
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducer: {},                   // методы для обновления нашего стейта
+});
+
