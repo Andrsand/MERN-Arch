6be64c07b0eadac74ed0217ac7c44e3965eee29d
andrsand <angorko@list.ru>
2023-07-10 20:32:20 +0300

    Implementation of the functionality of getting an article by id in FullPost.jsx

--- Arch-blog-frontend/src/pages/FullPost.jsx
+++ Arch-blog-frontend/src/pages/FullPost.jsx
@@ -1,52 +1,44 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import axios from '../axios';
 
 import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
-import axios from "axios";
 
 export const FullPost = () => {
-
-  const params = useParams();
-
   // делаем запрос не в Redax а локально, поскольку это вего лишь одна статья
   const [data, setData] = React.useState(); // тут просто хранится deta
+  const [isLoading, setLoading] = React.useState(true); // стейт на то что идет загрузка
   const { id } = useParams();
 
   React.useEffect(() => {
-    axios.get(`/posts/${id}`).then(res => { // делаем запрос с помощью axios на /posts/${id} 
+    axios.get(`/posts/${id}`).then((res) => {                // делаем запрос с помощью axios на /posts/${id} 
       setData(res.data);                    // полученный результат сохраняем в стейт
-    }).catch(err => {                       // если же была ошибка ....
-      console.warn(err);
+      setLoading(false);                    // если запрос выполнился успешно - то setLoading(false); т.е. указать, что загрузка завершилась
+    }).catch((err) => {                       // если же была ошибка ....
+      console.log(err);
       alert('Ошибка при получении статьи');
     });
   }, []);
 
-  return (
+  if (isLoading) {    // если идет загрузка то рендерим isLoading 
+    return <Post isLoading={isLoading} isFullPost />;  // isFullPost - объясняет, что это полная запись
+  }
+
+  return ( // если же загрузка завершена, то рендерим реальную информацию
     <>
       <Post
-        id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
+        id={data._id}
+        title={data.title}
         imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={{
-          avatarUrl:
-            "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
-        }}
-        createdAt={"12 июня 2022 г."}
-        viewsCount={150}
-        commentsCount={3}
-        tags={["react", "fun", "typescript"]}
-        isFullPost
-      >
-        <p>
-          Hey there! 👋 I'm starting a new series called "Roast the Code", where
-          I will share some code, and let YOU roast and improve it. There's not
-          much more to it, just be polite and constructive, this is an exercise
-          so we can all learn together. Now then, head over to the repo and
-          roast as hard as you can!!
-        </p>
+        user={data.user}     // юзер 
+        createdAt={data.createdAt}    // дата создания
+        viewsCount={data.viewsCount}  // число просмотров
+        commentsCount={3}            // комментарии
+        tags={data.tags}              // тэги
+        isFullPost>                   {/*// isFullPost - объясняет, что это полная запись*/}
+        {/*<p>{data.text}</p> текст*/}
       </Post>
       <CommentsBlock
         items={[
