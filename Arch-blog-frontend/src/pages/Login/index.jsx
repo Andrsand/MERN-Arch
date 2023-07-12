99e6e85ebaa0c83afe79efcdf146b0bb31f696b5
andrsand <angorko@list.ru>
2023-07-12 08:53:07 +0300

    Registering form fields for react-hook-form

--- Arch-blog-frontend/src/pages/Login/index.jsx
+++ Arch-blog-frontend/src/pages/Login/index.jsx
@@ -16,8 +16,9 @@ export const Login = () => {        // подключаем библиотеку
   } = useForm({
     defaultValues: {                // укапзываем изначальное состояние полей формы
       email: '',
-      password: ''
-    }
+      password: '',
+    },
+    mode: 'onChange',               // указываем, что валидация должна происходить только в случае изменения этих полей
   });
 
   const onSubmit = (values) => {  // эта функция выполняется только в случае коректной валидации
@@ -33,15 +34,24 @@ export const Login = () => {        // подключаем библиотеку
         <TextField
           className={styles.field}
           label="E-Mail"
-          error
-          helperText="Неверно указана почта"
+          error={Boolean(errors.email?.message)} // если errors.email ?.message получена то будет - true и будет подсветка красным
+          helperText={errors.email?.message}   // если есть ошибка то возьми параметр errors и вытащи message если нет - то не надо вытаскивать
+          type="email"
+          {...register('email', { required: 'Укажите почту' })} // если поле не заполнено - выходит надпись "укажите почту"
           fullWidth
         />
-        <TextField className={styles.field} label="Пароль" fullWidth />
-        <Button size="large" variant="contained" fullWidth>
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          error={Boolean(errors.password?.message)} // если errors.password ?.message получена то будет - true и будет подсветка красным
+          helperText={errors.password?.message}   // если есть ошибка то возбми параметр errors и вытащи message если нет - то не надо вытаскивать
+          {...register('password', { required: 'Укажите пароль' })} // если поле не заполнено - выходит надпись "укажите почту"
+          fullWidth />
+        <Button type="submit" size="large" variant="contained" fullWidth>
           Войти
         </Button>
       </form>
     </Paper>
   );
 };
+
