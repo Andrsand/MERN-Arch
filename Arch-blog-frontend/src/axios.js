284a7a4dc99b8c44bf026e9f7f47619ea6485c23
andrsand <angorko@list.ru>
2023-07-14 08:56:47 +0300

    Implementation of sending an authorization token to the backend in axios.js

--- Arch-blog-frontend/src/axios.js
+++ Arch-blog-frontend/src/axios.js
@@ -4,4 +4,10 @@ const instance = axios.create({ // объясняем axios что он долж
     baseURL: 'http://localhost:4444' // объясняем что нужно делать всегда запросы на http://localhost:4444
 });
 
+instance.interceptors.request.use((config) => {
+    config.headers.Authorization = window.localStorage.getItem('token');
+    return config;
+});
+
+
 export default instance;
