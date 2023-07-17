2855f22e445b63b5e104f8dde3dc0351ebaff213
andrsand <angorko@list.ru>
2023-07-17 08:53:41 +0300

    Implementation of image search for preview

--- Arch-blog-frontend/src/pages/AddPost/index.jsx
+++ Arch-blog-frontend/src/pages/AddPost/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -8,6 +9,7 @@ import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
 import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
 import styles from './AddPost.module.scss';
 
 export const AddPost = () => {
@@ -16,8 +18,20 @@ export const AddPost = () => {
   const [value, setValue] = React.useState('');
   const [title, setTitle] = React.useState('');
   const [tags, setTags] = React.useState('');
+  const inputFileRef = React.useRef(null);
 
-  const handleChangeFile = () => { };
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      console.log(data);
+    } catch (err) {
+      console.warn(err);
+      alert('Ошибка при загрузке файла!');
+    }
+  };
 
   const onClickRemoveImage = () => { };
 
@@ -46,10 +60,10 @@ export const AddPost = () => {
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
         Загрузить превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
       {imageUrl && (
         <Button variant="contained" color="error" onClick={onClickRemoveImage}>
           Удалить
